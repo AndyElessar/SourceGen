1827d9e148970321b3ff0fb3b6563eb7fe0c20bd
Andy Elesser <andy090517@gmail.com>
2026-03-01 16:42:48 +0800

    Add SourceGen.Validation changes

---
+++ src/Validation/src/SourceGen.Validation.SourceGenerator/Generator/ValidationSourceGenerator.cs
@@ -0,0 +1,11 @@
+
+namespace SourceGen.Validation;
+
+[Generator(LanguageNames.CSharp)]
+internal sealed partial class ValidationSourceGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Validation/src/SourceGen.Validation.SourceGenerator/GlobalUsing.cs
@@ -0,0 +1,10 @@
+global using System.Collections.Immutable;
+global using System.Diagnostics;
+global using System.Diagnostics.CodeAnalysis;
+global using System.Runtime.CompilerServices;
+global using System.Text;
+global using System.Linq;
+global using Microsoft.CodeAnalysis;
+global using Microsoft.CodeAnalysis.CSharp;
+global using Microsoft.CodeAnalysis.CSharp.Syntax;
+global using PolyType.Roslyn;
